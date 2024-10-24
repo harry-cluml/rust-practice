3474a4d09f42d9d9522bf45bf6a7d3aaffabd6b2
harry-cluml <harry@cluml.com>
2024-10-24 17:44:07 +0900

    Add problem 1480 solution and examples
    close #5

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+mod problem_1480;
 mod problem_1929;
 mod problem_2235;
---
+++ src/problem_1480.rs
@@ -0,0 +1,37 @@
+fn solve(nums: &[i32]) -> Vec<i32> {
+    nums.iter()
+        .scan(0, |state, &x| {
+            *state += x;
+            Some(*state)
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let nums = [1, 2, 3, 4];
+        let expected_output = [1, 3, 6, 10];
+        let output = solve(&nums);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn example_2() {
+        let nums = [1, 1, 1, 1, 1];
+        let expected_output = [1, 2, 3, 4, 5];
+        let output = solve(&nums);
+        assert_eq!(output, expected_output);
+    }
+
+    #[test]
+    fn example_3() {
+        let nums = [3, 1, 2, 10, 1];
+        let expected_output = [3, 4, 6, 16, 17];
+        let output = solve(&nums);
+        assert_eq!(output, expected_output);
+    }
+}
