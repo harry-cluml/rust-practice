8db685053d11fd75e3b470e9e2bbc512b9b63b1c
Harry <harry@cluml.com>
2024-10-28 14:35:16 +0900

    Add problem 2114 solution and examples
    close #25

---
+++ src/problem_2114.rs
@@ -0,0 +1,33 @@
+#[allow(dead_code)]
+fn solve(sentences: &[&str]) -> Result<usize, String> {
+    sentences
+        .iter()
+        .map(|&x| x.split(' ').count())
+        .max()
+        .ok_or_else(|| format!("Must exist max value"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let sentences = [
+            "alice and bob love leetcode",
+            "i think so too",
+            "this is great thanks very much",
+        ];
+        let expected_output = 6;
+        let output = solve(&sentences);
+        assert_eq!(output.unwrap(), expected_output);
+    }
+
+    #[test]
+    fn example_2() {
+        let sentences = ["please wait", "continue to fight", "continue to win"];
+        let expected_output = 3;
+        let output = solve(&sentences);
+        assert_eq!(output.unwrap(), expected_output);
+    }
+}
