d98082ca9e553a891a73577aff6d8ef4a2718b65
Harry <harry@cluml.com>
2024-10-28 14:29:48 +0900

    Add Github Actions CI
    Added ci.yml received via link
    Added if false line to Upload coverage to Codecov step
    
    close #29

--- src/problem_1281.rs
+++ src/problem_1281.rs
@@ -1,5 +1,6 @@
 use std::iter::successors;
 
+#[allow(dead_code)]
 fn solve(n: i32) -> Result<i32, String> {
     let (prod, sum) = successors(Some(n), |&x| if x > 0 { Some(x / 10) } else { None })
         .filter_map(|x| if x > 0 { Some(x % 10) } else { None })
@@ -7,21 +8,21 @@ fn solve(n: i32) -> Result<i32, String> {
             (
                 prod.and_then(|p: i32| {
                     p.checked_mul(x)
-                        .ok_or_else(|| format!("Overflow occurred. prod: {} x: {}", p, x))
+                        .ok_or_else(|| format!("Overflow occurred. prod: {p} x: {x}"))
                 }),
                 sum.and_then(|s: i32| {
                     s.checked_add(x)
-                        .ok_or_else(|| format!("Overflow occurred. sum: {} x: {}", s, x))
+                        .ok_or_else(|| format!("Overflow occurred. sum: {s} x: {x}"))
                 }),
             )
         });
     match (prod, sum) {
         (Ok(p), Ok(s)) => p
             .checked_sub(s)
-            .ok_or_else(|| format!("Overflow occurred. prod: {} sum: {}", p, s)),
+            .ok_or_else(|| format!("Overflow occurred. prod: {p} sum: {s}")),
         (Ok(_), Err(s)) => Err(s),
         (Err(p), Ok(_)) => Err(p),
-        (Err(p), Err(s)) => Err(format!("{}\n{}", p, s)),
+        (Err(p), Err(s)) => Err(format!("{p}\n{s}")),
     }
 }
 
